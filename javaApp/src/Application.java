2efe8c8606678fd300350fa1ae9eb35b6fe3b118
Lebr0N JamS <duartepadrao20@gmail.com>
2023-09-22 23:55:51 +0100

    ok

--- javaApp/src/Application.java
+++ javaApp/src/Application.java
@@ -2,24 +2,8 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String operation = scanner.nextLine();
         int a = scanner.nextInt();
         int b = scanner.nextInt();
 
-        if (operation.equals("sum")) {
-            System.out.println(a + b);
-        }
-
-        if (operation.equals("div")) {
-            System.out.println(a / b);
-        }
-
-        if (operation.equals("sub")) {
-            System.out.println(a - b);
-        }
-
-        if (operation.equals("mul")) {
-            System.out.println(a * b);
-        }
-    }
+        System.out.println(a + b);
 }
